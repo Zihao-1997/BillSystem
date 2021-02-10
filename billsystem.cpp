63ac0aa0ffd23288dfdab19bf2c8c0012785e725
ZiHao <zihao_1997@outlook.com>
2021-02-10 21:37:08 +0800

    create new project

---
+++ billsystem.cpp
@@ -0,0 +1,11 @@
+#include "billsystem.h"
+
+BillSystem::BillSystem(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+BillSystem::~BillSystem()
+{
+}
+
---
+++ billsystem.h
@@ -0,0 +1,14 @@
+#ifndef BILLSYSTEM_H
+#define BILLSYSTEM_H
+
+#include <QWidget>
+
+class BillSystem : public QWidget
+{
+    Q_OBJECT
+
+public:
+    BillSystem(QWidget *parent = nullptr);
+    ~BillSystem();
+};
+#endif // BILLSYSTEM_H
